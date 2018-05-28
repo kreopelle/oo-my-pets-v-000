68d5d3f5229ad50515f4049205700de102449c6e
Kayla Reopelle <kreopelle@gmail.com>
2018-05-28 16:37:51 +0000

    automatically backed up by learn

--- spec/owner_spec.rb
+++ spec/owner_spec.rb
@@ -150,6 +150,7 @@ describe Owner do
           :cats => [Cat.new("Mittens"), tabby]
         }
         owner.sell_pets
+        binding.pry 
         owner.pets.each {|type, pets| expect(pets.empty?).to eq(true) }
         [fido, tabby, nemo].each { |o| expect(o.mood).to eq("nervous") }
       end
